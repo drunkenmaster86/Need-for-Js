e68fe1975800f5d7e121e49d13506e0c20885d60
drunkenmaster86 <sergej.romanchenko.86@bk.ru>
2020-09-10 17:05:16 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const score = document.querySelector(".score"),
+  start = document.querySelector(".start"),
+  gameArea = document.querySelector(".gameArea"),
+  car = document.createElement("div");
+
+car.classList.add("car");
+
+start.addEventListener("click", startGame);
+document.addEventListener("keydown", startRun);
+document.addEventListener("keyup", stopRun);
+
+const keys = {
+  ArrowUp: false,
+  ArrowDown: false,
+  ArrowLeft: false,
+  ArrowRight: false,
+};
+
+const setting = {
+  start: false,
+  score: 0,
+  speed: 3,
+};
+
+function startGame() {
+  start.classList.add("hide");
+  setting.start = true;
+  gameArea.appendChild(car);
+  setting.x = car.offsetLeft;
+  setting.y = car.offsetTop;
+  requestAnimationFrame(playGame);
+}
+
+function playGame() {
+  console.log("Play game!");
+  if (setting.start) {
+    if (keys.ArrowLeft && setting.x > 0) {
+      setting.x -= setting.speed;
+    }
+    if (keys.ArrowRight) {
+      setting.x += setting.speed;
+    }
+    if (keys.ArrowUp && setting.y > 0) {
+      setting.y -= setting.speed;
+    }
+    if (keys.ArrowDown) {
+      setting.y += setting.speed;
+    }
+    car.style.left = setting.x + "px";
+    car.style.top = setting.y + "px";
+    requestAnimationFrame(playGame);
+  }
+}
+
+function startRun(event) {
+  event.preventDefault();
+  keys[event.key] = true;
+}
+
+function stopRun(event) {
+  event.preventDefault();
+  keys[event.key] = false;
+}
